36e1c064c419fb5fe2f70535fa67110d93016d09
AuroreSifflet <aurore.sifflet@gmail.com>
2024-03-04 11:52:51 +0100

    Documenter le code

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -3,6 +3,16 @@ window.addEventListener('DOMContentLoaded',()=>{
   const pokeDiv=document.getElementById('pokemon-info')
   const pokeAbilityBtn=document.getElementById('ability')
   
+  /**
+   * Fonction asynchrone fetchPokemon() 
+   * qui va chercher aléatoirement un pokemon grâce au pokedexNum sur l'api pokeapi 
+   * et assigne sa valeur à foundPokemon
+   * si foundPokemon existe, création de son nom, sa valeur est assignée à pokeInfo.name
+   * si le paragraphe p pokeP n'est pas vide, alors on le vide, 
+   * avant d'ajouter le texte 'Your Pokémon is' avec le nom du pokemon pokeInfo.name  
+   * enfin il retire l'attribut disabled au bouton pokeAbilityBtn
+   * @returns undefined, il n'y a pas de valeur de retour
+   */
   const fetchPokemon=async ()=>{
     const pokedexNum=Math.floor(Math.random() * 897)
     let foundPokemon=''
@@ -13,7 +23,7 @@ window.addEventListener('DOMContentLoaded',()=>{
       foundPokemon=await fetch(`https://pokeapi.co/api/v2/pokemon/${pokedexNum}`, {method: 'GET', headers: {'Content-Type': 'application/json'}})
     }
     catch(error){console.error(error.message)}
-
+    // Si un pokemon a été trouvé création du nom pokemon pokeInfo 
     if(foundPokemon){
       try{
         jsonPokemon=await foundPokemon.json()
@@ -23,12 +33,22 @@ window.addEventListener('DOMContentLoaded',()=>{
 
     }
     else{jsonPokemon='No Pokémon found...'}
-
+    
     if(pokeP.innerText !== ''){pokeP.innerText=''}
     pokeP.innerText=`Your Pokémon is ${pokeInfo.name}.`
     pokeAbilityBtn.removeAttribute('disabled')
   }
-
+  
+  /**
+   * Fonction asynchrone fetchPokemonAbilities() 
+   * qui va chercher aléatoirement un pokemon grâce au pokedexNum sur l'api pokeapi 
+   * et assigne sa valeur à foundAbilities
+   * si un pokemon a été trouvé, foundAbilities existe, 
+   * récupération et mise en forme de son nom jsonAbilities.name
+   * sa valeur est assignée à abilityToDisplay qui sera affichée dan sle paragraphe pokeAbility
+   * 
+   * @returns undefined, il n'y a pas de valeur de retour
+   */
   const fetchPokemonAbilities=async()=>{
     const pokedexNum=Math.floor(Math.random() * 266)
     let foundAbilities=''
@@ -59,17 +79,35 @@ window.addEventListener('DOMContentLoaded',()=>{
     pokeAbility.innerText=`It now knows the move ${abilityToDisplay}!`
   }
   
+  /**
+   * Fonction anonyme invoquePokemon() 
+   * qui sélectionne le bouton id pokemon
+   * qui permet lors de l'événement click d'executer la fonction fetchPokemon
+   * le résultat de l'execution s'affiche sur la page index.html dans la div pokemon-info
+   * @returns undefined, il n'y a pas de valeur de retour
+   */
   const invoquePokemon=()=>{
     const pokeBtn=document.getElementById('pokemon')
     pokeBtn.addEventListener('click', fetchPokemon)
     pokeDiv.appendChild(pokeP)
   }
 
+    /**
+   * Fonction anonyme pokemonAbility() 
+   * qui permet lors de l'événement click 
+   * d'executer la fonction fetchPokemonAbilities grâce au bouton id ability de la page index.html
+   * le résultat de l'execution s'affiche sur la page index.html dans la div pokemon-info
+   * @returns undefined, il n'y a pas de valeur de retour
+   */
   const pokemonAbility=()=>{
     pokeAbilityBtn.addEventListener('click', fetchPokemonAbilities)
-    pokeDiv.appendChild(pokeAbility)
+    pokeDiv.appendChild(pokeAbilityBtn)
   }
-  
+  /**
+   * Fonction IIFE startAll() qui exécute immédiatement au chargement du fichier fetchData.js 
+   * les fonctions invoquePokemon() et pokemonAbility()
+   * @returns undefined, il n'y a pas de valeur de retour
+   */
   (function startAll(){
     invoquePokemon()
     pokemonAbility()
--- public/js/functionsToTest.js
+++ public/js/functionsToTest.js
@@ -1,3 +1,12 @@
+
+/**
+ * Fonction anonyme returnAnObject()
+ * 
+ *  si args n'ont pas été donnés, un message est retourné
+ * @param {*} args
+ * @return {Object | string} Un objet est retourné avec comme propriété un nombre et comme valeur un arg
+ * cependant si args n'ont pas été donnés, un message est retourné
+ */
 const returnAnObject = (...args) => {
   let response = {};
   if (args.length) {
@@ -12,11 +21,21 @@ const returnAnObject = (...args) => {
   return response;
 };
 
+
+/**
+ * Fonction anonyme multiplyAllByTwo()
+ * permet de mapper sur le tableau arrayOfNumbers qui est passé en argument
+ * @param {Array<number>} arrayOfNumbers 
+ * @return {Array<number> | string} Un nouveau tableau de nombre response est retourné 
+ * avec chaque nombre du tableau arrayOfNumbers multiplié par deux
+ * cependant si arrayOfNumbers fournit n'était pas un tableau, un message est retourné
+ */
 const multiplyAllByTwo = (arrayOfNumbers) => {
   let response;
   if (arrayOfNumbers.constructor.prototype === new Array().constructor.prototype) {
     response = arrayOfNumbers.map((val => val * 2));
     console.log('arrayTimesTwo: ', arrayTimesTwo);
+    console.log('response: ', response);
   } else {
     response = 'The argument is not an Array of numbers';
   };
